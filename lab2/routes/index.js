76f49dca106c75ba659d41c9af0b2ee81dbdf6c4
Vyacheslav Nagel <v9gel@mail.ru>
2020-02-17 23:49:21 +0700

    * отрисовка главной и админки

--- lab2/routes/index.js
+++ lab2/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 // главная, список всех проектов
 router.get('/', function(req, res, next) {
-  res.render('index', { isAdmin: false });
+  res.render('index', { title: 'Главная', isAdmin: false });
 });
 
 // админка
 router.get('/admin', function(req, res, next) {
-  res.render('index', { isAdmin: true });
+  res.render('index', { title: 'Админка', isAdmin: true });
 });
 
 module.exports = router;
