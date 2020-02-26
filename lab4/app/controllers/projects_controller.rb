688b3266055dc74f5a97222dcdcb45524472dea4
Vyacheslav Nagel <v9gel@mail.ru>
2020-02-27 04:47:26 +0700

    + главная, админка, просмотр с шаблоном

--- lab4/app/controllers/projects_controller.rb
+++ lab4/app/controllers/projects_controller.rb
@@ -1,11 +1,20 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def admin
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
+    @material = Material.find(@project.id)
+    @buildings = []
+
+    ProjectBuilding.where(project_id: params[:id]).each do |building|
+      @buildings.push(Building.find(building.building_id).name)
+    end
   end
 
   def edit
